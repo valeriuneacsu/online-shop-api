4bb67d4fea4060c645c475460baf561c2ce2cf41
valeriu.neacsu <valeriu.neacsu@yahoo.com>
2019-06-01 17:39:24 +0300

    update and delete

--- src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
@@ -5,8 +5,10 @@ import org.fasttrackit.onlineshopapi.domain.Product;
 import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.repository.ProductRepository;
 import org.fasttrackit.onlineshopapi.transfer.CreateProductRequest;
+import org.fasttrackit.onlineshopapi.transfer.UpdateProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -50,6 +52,24 @@ public class ProductService {
         return productRepository.findById(id)
                 // using Lambda expressions
                 .orElseThrow(() -> new ResourceNotFoundException("Product " + id + " does noy exist."));
+    }
+
+
+    public Product updateproduct(long id, UpdateProductRequest request) throws ResourceNotFoundException {
+        LOGGER.info("Updating product{} with {]", id, request);
+
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+
+        return productRepository.save(product);
+    }
+
+
+    public void deleteProduct(long id) {
+        LOGGER.info("Deleting product {]", id);
+        productRepository.deleteById(id);
+        LOGGER.info("Deleted product {]", id);
     }
 
 
