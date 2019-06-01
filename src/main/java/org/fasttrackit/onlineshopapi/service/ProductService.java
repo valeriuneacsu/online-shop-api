0e9542dd69fe6d38c4d73fe6bb5eea3d11e9312a
valeriu.neacsu <valeriu.neacsu@yahoo.com>
2019-06-01 17:24:58 +0300

    object mapper example

--- src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
@@ -1,5 +1,6 @@
 package org.fasttrackit.onlineshopapi.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.fasttrackit.onlineshopapi.domain.Product;
 import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.repository.ProductRepository;
@@ -18,21 +19,26 @@ public class ProductService {
 
     //IoC (inversion of control) and dependency injection
     private final ProductRepository productRepository;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, ObjectMapper objectMapper) {
         this.productRepository = productRepository;
+        this.objectMapper = objectMapper;
     }
 
     public Product createProduct(CreateProductRequest request){
 
         LOGGER.info("Creating product {}",request );  // in loc de .info se poate pune .debug, .error
 
-        Product product = new Product();
-        product.setName(request.getName());
-        product.setPrice(request.getPrice());
-        product.setQuantity(request.getQuantity());
-        product.setImagePath(request.getImagePath());
+
+        Product product = objectMapper.convertValue(request, Product.class);
+        // same result as above with objectMapper
+//        Product product = new Product();
+//        product.setName(request.getName());
+//        product.setPrice(request.getPrice());
+//        product.setQuantity(request.getQuantity());
+//        product.setImagePath(request.getImagePath());
 
        return productRepository.save(product);
 
